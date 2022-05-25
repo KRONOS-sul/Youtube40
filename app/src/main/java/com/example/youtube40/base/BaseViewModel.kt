9a0271ddebd8b0de952e7349dba80bddcda40a29
Sultan <93419011+KRONOS-sul@users.noreply.github.com>
2022-05-25 22:51:23 +0300

    если есть проблема то все вопросы к альберту

--- app/src/main/java/com/example/youtube40/base/BaseViewModel.kt
+++ app/src/main/java/com/example/youtube40/base/BaseViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.youtube40.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 open class BaseViewModel: ViewModel() {
+    val loading = MutableLiveData<Boolean>()
 }
